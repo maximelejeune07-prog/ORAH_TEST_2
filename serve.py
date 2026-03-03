615c29f19a9fbda6acfd89fd8e90ce2bbc8c01de
maximelejeune07-prog <maxime.lejeune07@gmail.com>
2026-03-03 20:19:42 +0100

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import os, sys
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import http.server, socketserver
+PORT = 3000
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"ORAH Website running at http://localhost:{PORT}")
+    httpd.serve_forever()
